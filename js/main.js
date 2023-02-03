7d50ddf2379664b12c5e516fc37e3c745ddcf101
Pungushe <y.borozna81@internet.ru>
2023-02-03 17:25:21 +0300

    Nasa

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import Swiper from "swiper";
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView:2,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-right",
+        prevEl: ".swiper-button-left",
+    },
+});
